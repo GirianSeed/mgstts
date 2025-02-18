f66adbb3d69c94a1b6c893441a5c4d5351e8c467
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-17 19:52:54 -0500

    Create chara.h

---
+++ source/include/chara.h
@@ -0,0 +1,16 @@
+#ifndef __MGSTTS_CHARA_H__
+#define __MGSTTS_CHARA_H__
+
+#include "strcode.h"
+
+// XXX The empty parameter list is widely regarded as one of old C's
+// XXX anti-features. C++ and C23 have changed its meaning to (void).
+
+typedef void *(*NEWCHARA)();
+
+typedef struct {
+    unsigned int class_id;
+    NEWCHARA func;
+} CHARA;
+
+#endif // {{{ END OF FILE }}}
