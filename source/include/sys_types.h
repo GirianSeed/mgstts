cfe5c580dd3fa76002d548d72ea25f780d5d4e1c
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-04-30 17:27:14 -0400

    sys_types.h: fix preprocessor syntax

--- source/include/sys_types.h
+++ source/include/sys_types.h
@@ -12,7 +12,7 @@
 // NOTE: sizeof(long) differs between ILP32/LLP64 and LP64 systems.
 // ...and the Emotion Engine's IP32L64 data model.
 
-#ifndef _SYS_TYPES_H || __GNUC_PREREQ(2, 96)
+#if !defined(_SYS_TYPES_H) || __GNUC_PREREQ(2, 96)
 // NOTE: Apparently GCC versions before 2.96 can't cope with typedefs
 // being duplicately defined, so we're not defining anything if the
 // compiler isn't new enough.
@@ -22,8 +22,8 @@
 // MUST be included before <eetypes.h> when using ee-gcc versions earlier
 // than 2.96. This problem was fixed in SDK Release 3.0.
 
-#ifndef _POSIX_SOURCE
-#ifndef _BSDTYPES_DEFINED && !defined(_WINSOCK_H)
+#if !defined(_POSIX_SOURCE)
+#if !defined(_BSDTYPES_DEFINED) && !defined(_WINSOCK_H)
 typedef unsigned char   u_char;
 typedef unsigned short  u_short;
 typedef unsigned int    u_int;
